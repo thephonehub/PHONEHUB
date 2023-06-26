6e1e3f67d2652a8cfdd58ee9c683552bfdfed218
thephonehub <137811337+thephonehub@users.noreply.github.com>
2023-06-26 22:51:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const toggleSwitch = document.getElementById('dark-mode-toggle');
+const body = document.body;
+
+if (localStorage.getItem('darkMode') === 'enabled') {
+  toggleSwitch.checked = true;
+  body.classList.add('dark-mode');
+}
+
+toggleSwitch.addEventListener('change', function(event) {
+  if (event.target.checked) {
+    body.classList.add('dark-mode');
+    localStorage.setItem('darkMode', 'enabled');
+  } else {
+    body.classList.remove('dark-mode');
+    localStorage.setItem('darkMode', null);
+  }
+});
