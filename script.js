1cba33f3c47a965b7ed6eb1f95b26a3561528cdf
thephonehub <137811337+thephonehub@users.noreply.github.com>
2023-08-03 01:21:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,16 @@ toggleSwitch.addEventListener('change', function(event) {
     localStorage.setItem('darkMode', null);
   }
 });
+
+
+
+
+ Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
