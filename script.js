6de64e58e504cd3435be4d49c5069b85bf17ea47
thephonehub <137811337+thephonehub@users.noreply.github.com>
2023-08-04 22:13:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,56 @@ toggleSwitch.addEventListener('change', function(event) {
 
 
 
+
+
+const slider = document.querySelector('.slider');
+let isDragging = false;
+let startPos = 0;
+let currentTranslate = 0;
+let prevTranslate = 0;
+let animationID;
+
+slider.addEventListener('touchstart', touchStart);
+slider.addEventListener('touchmove', touchMove);
+slider.addEventListener('touchend', touchEnd);
+slider.addEventListener('touchcancel', touchEnd);
+
+function touchStart(event) {
+  startPos = event.touches[0].clientX;
+  isDragging = true;
+  animationID = requestAnimationFrame(animation);
+  slider.style.transition = 'none';
+}
+
+function touchMove(event) {
+  if (!isDragging) return;
+  const currentPosition = event.touches[0].clientX;
+  const moveAmount = currentPosition - startPos;
+  currentTranslate = prevTranslate + moveAmount;
+}
+
+function touchEnd() {
+  isDragging = false;
+  cancelAnimationFrame(animationID);
+
+  const threshold = window.innerWidth / 4;
+  if (Math.abs(currentTranslate) > threshold) {
+    if (currentTranslate > 0) {
+      prevTranslate += window.innerWidth;
+    } else {
+      prevTranslate -= window.innerWidth;
+    }
+  }
+
+  slider.style.transition = 'transform 0.3s ease-out';
+  slider.style.transform = `translateX(${prevTranslate}px)`;
+}
+
+function animation() {
+  slider.style.transform = `translateX(${currentTranslate}px)`;
+  animationID = requestAnimationFrame(animation);
+}
+
+
+
+
