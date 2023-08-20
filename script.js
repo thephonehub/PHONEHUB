ad7fdf7620dbeacf13d929d01af1f86bf55243f7
thephonehub <137811337+thephonehub@users.noreply.github.com>
2023-08-21 02:14:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+  const productCards = document.querySelectorAll('.product-card');
+
+  productCards.forEach(card => {
+    card.addEventListener('touchstart', () => {
+      card.classList.add('sparkling');
+    });
+
+    card.addEventListener('touchend', () => {
+      card.classList.remove('sparkling');
+    });
+  });
+});
 
