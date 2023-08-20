ecf326e79d1467b6e6f391cf0a17f03f67af3586
thephonehub <137811337+thephonehub@users.noreply.github.com>
2023-08-21 00:37:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,29 +20,3 @@ toggleSwitch.addEventListener('change', function(event) {
 }
 
 
-
-// script.js
-document.addEventListener('DOMContentLoaded', function() {
-  const productCard = document.querySelector('.product-card');
-  const productImage = productCard.querySelector('.product-image');
-  
-  const parallaxStrength = 10; // Adjust this value to control parallax effect
-  
-  productCard.addEventListener('mousemove', (event) => {
-    const cardRect = productCard.getBoundingClientRect();
-    const mouseX = event.clientX - cardRect.left;
-    const mouseY = event.clientY - cardRect.top;
-
-    const offsetX = (mouseX - cardRect.width / 2) / cardRect.width * parallaxStrength;
-    const offsetY = (mouseY - cardRect.height / 2) / cardRect.height * parallaxStrength;
-
-    productImage.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
-  });
-
-  productCard.addEventListener('mouseleave', () => {
-    productImage.style.transform = 'translate(0, 0)';
-  });
-});
-
-
-
